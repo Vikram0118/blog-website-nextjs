c0cd5034d017ebd5a321404bee6bcbf26b41a189
Vikram0118 <vikramgit001@gmail.com>
2024-01-01 15:22:43 +0530

    final test - font optimizing

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -2,12 +2,19 @@ import Link from 'next/link'
 import Image from 'next/image'
 import NavItems from './NavItems'
 import MobileNav from './MobileNav'
+import { Anton } from 'next/font/google'
+
+const anton = Anton({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-poppins'
+})
 
 export default function Navbar() {
   return (
     <nav className='w-full border fixed top-0 z-50 py-2 text-black bg-white'>
       <div className='flex-between mx-auto w-full max-w-screen-xl px-6 xs:px-8 sm:px-16'>
-        <Link href='/' className='anton-font text-5xl'> VP. </Link>
+        <Link href='/' className={`anton-font text-5xl ${anton.className}`}> VP. </Link>
         <nav className="md:flex-between hidden">
           <NavItems />
         </nav>
